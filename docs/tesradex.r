8c929dff2053f0282b40bc384778dbae85f76e5b
realchainlife <realchainlife@protonmail.com>
2018-10-26 10:58:54 +0300

    add tesradex.r

---
+++ docs/tesradex.r
@@ -0,0 +1,29 @@
+Kucoin API
+==========
+
+client module
+----------------------
+
+.. automodule:: kucoin.client
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
+exceptions module
+--------------------------
+
+.. automodule:: kucoin.exceptions
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
+helpers module
+--------------------------
+
+.. automodule:: kucoin.helpers
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
