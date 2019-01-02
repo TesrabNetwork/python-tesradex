46a110739aaf8f1728424ff93b8c3dfabc382280
realchainlife <realchainlife@protonmail.com>
2019-01-02 10:10:45 +0300

    update client.py function descriptions

--- tesradex/client.py
+++ tesradex/client.py
@@ -1512,7 +1512,7 @@ class Client(object):
 
         https://tesradexapidocs.docs.apiary.io/#reference/0/trading/order-details
 
-        :param symbol: Name of symbol e.g. KCS-BTC
+        :param symbol: Name of symbol e.g. TDEX-BTC
         :type symbol: string
         :param order_type: Order type
         :type order_type: string
